4cc82302449e94b23c629f50cc39802b9a8677a9
LinkShake <nicolacampo19@gmail.com>
2022-09-06 20:11:30 +0200

    fixed UI, resize bugs

--- src/components/home/InitialPage.jsx
+++ src/components/home/InitialPage.jsx
@@ -4,13 +4,17 @@ export default function InitialPage() {
   const introductionText = "Hi! I'm Nicola Del Pio Luogo";
 
   return (
-    <div className="initial-page">
-      <h1>{introductionText}</h1>
-      <h2>
-        I'm an Italian high-school student with a great passion for full-stack
-        web development
-      </h2>
-      <p className="sign">LinkShake</p>
-    </div>
+    <>
+      <div className="initial-page">
+        <h1>{introductionText}</h1>
+        <h2>
+          I'm an Italian high-school student with a great passion for full-stack
+          web development
+        </h2>
+      </div>
+      <p className="sign" style={{ marginLeft: "10vw" }}>
+        LinkShake
+      </p>
+    </>
   );
 }
